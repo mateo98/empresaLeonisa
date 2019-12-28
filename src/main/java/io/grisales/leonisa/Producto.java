cac70c67e8948d2500f5153331d7be444403bb7f
magrisales <magrisales@personalsoft.com.co>
2019-12-28 10:20:50 -0500

    Se diseña la clase producto

---
+++ src/main/java/io/grisales/leonisa/Producto.java
@@ -0,0 +1,11 @@
+package io.grisales.leonisa;
+
+public class Producto {
+    private String ciu;
+    private double precio;
+
+    public Producto(String ciu, double precio){
+        this.ciu = ciu;
+        this.precio = precio;
+    }
+}
